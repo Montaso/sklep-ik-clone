4bcbdd9a7258036afc4e375c60c2d1417d3a48f4
Misphe <124389712+Misphe@users.noreply.github.com>
2024-12-02 17:03:15 +0100

    Squashed commit of the following:
    commit f7cefc8d52dac05e30d3c4e2bfd20c423898f5ab
    Merge: de94f4b1 b1258469
    Author: Misphe <124389712+Misphe@users.noreply.github.com>
    Date:   Mon Dec 2 17:02:14 2024 +0100
    
        Merge branch '13-prestashop-prestashop-setup' into Add-scrapped-products-to-prestashop
    
    commit de94f4b1ee3fac5fedd7ccf7eea8c95701449944
    Author: Misphe <124389712+Misphe@users.noreply.github.com>
    Date:   Mon Dec 2 16:50:02 2024 +0100
    
        Added adding products via rest api
    
    commit f31a14cfee1ccde69f93cc06ebdbc93101c70bea
    Author: Misphe <124389712+Misphe@users.noreply.github.com>
    Date:   Sun Dec 1 18:48:32 2024 +0100
    
        prestashop setup (#15)
    
        * prestashop config
    
        * janek tests
    
        * gitignore change
    
        * Delete shop/prestashop directory
    
        * update docker
    
        * update docker
    
        * Updated docker-compose.yml
    
        fixed connection to db
    
        * working presta
    
        * added local database
    
        * ssl works
    
        * add presta src
    
        * add db dump
    
        * Delete shop/src/General**
    
        Bad file name
    
        * add dbdump folder
    
        * changes gitignore
    
        * started refactoring ppage
    
        * db dump
    
        * docker-compose file for mac config
    
        * some shop changes
    
        * Updated aa_data_loader scripts
    
        Corrected the product addition to prestashop via API
    
        ---------
    
        Co-authored-by: ar1valdi <jkaczerski@gmail.com>
        Co-authored-by: ar1valdi <127351075+ar1valdi@users.noreply.github.com>
        Co-authored-by: Montaso <28571739+Montaso@users.noreply.github.com>

--- shop/aa_data_loader/send.py
+++ shop/aa_data_loader/send.py
@@ -1,6 +1,9 @@
+import os
+
 import requests
 from requests.auth import HTTPBasicAuth
 import urllib3
+import xml.etree.ElementTree as ET
 
 # disable self-signed ssl warning
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -23,6 +26,65 @@ def get_blank_schema(endpoint: str):
     )
     return response.raw
 
+def download_image(image_url: str, file_name: str):
+    response = requests.get(image_url)
+    if response.status_code == 200:
+        with open(file_name, 'wb') as file:
+            file.write(response.content)
+        return True
+    else:
+        print(f"Failed to download image from {image_url}. Status Code: {response.status_code}")
+        return False
+
+
+def upload_product_image(product_id: int, image_file_path: str):
+    image_upload_url = f"{base_url}products/{product_id}/images"
+
+    with open(image_file_path, 'rb') as image_file:
+        files = {'file': (image_file_path, image_file, 'image/jpeg')}
+        response = requests.post(image_upload_url, files=files, headers=headers, auth=HTTPBasicAuth(api_key, ""), verify=False)
+
+    if response.status_code == 200:
+        print(f"Image uploaded successfully for product {product_id}!")
+    else:
+        print(f"Failed to upload image for product {product_id}. Status Code: {response.status_code}")
+        print(response.text)
+
+
+def upload_product_images(product_id: int, img_uris: list):
+    for img_url in img_uris:
+        img_url = img_url.strip('[]\'\" ')
+        file_name = img_url.split("/")[-1]
+
+        if download_image(img_url, file_name):
+            upload_product_image(product_id, file_name)
+            os.remove(file_name)
+        else:
+            print(f"Skipping upload for {img_url} due to download failure.")
+
+
+def send(payload: str, endpoint: str, img_uris):
+    response = requests.post(
+        f"{base_url}{endpoint}",
+        headers=headers,
+        data=payload.encode('utf-8'),
+        auth=HTTPBasicAuth(api_key, ""),
+        verify=False
+    )
+
+    if response.status_code == 201:
+        print("Product added successfully!")
+        root = ET.fromstring(response.text)
+        product_id = int(root.find(".//id").text) 
+        print("Product ID:", product_id)
+        if product_id:
+            upload_product_images(product_id, img_uris)
+        else:
+            print('no id')
+    else:
+        print("Failed to add product.")
+        print(f"Status Code: {response.status_code}")
+        print(response.text)
 
 def send_get(endpoint: str):
     return requests.get(
---
+++ shop/src/var/cache/dev/smarty/compile/classic/c0/7b/8e/c07b8e25d8968f5b6b55b8109a92c43eeb13f436_0.file.popup.tpl.php
@@ -0,0 +1,29 @@
+<?php
+/* Smarty version 3.1.48, created on 2024-12-01 15:00:33
+  from '/var/www/html/modules/welcome/views/templates/popup.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.48',
+  'unifunc' => 'content_674c6c01ad5a32_72839077',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'c07b8e25d8968f5b6b55b8109a92c43eeb13f436' => 
+    array (
+      0 => '/var/www/html/modules/welcome/views/templates/popup.tpl',
+      1 => 1676360366,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_674c6c01ad5a32_72839077 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<div class="onboarding-popup bootstrap">
+  <div class="content"></div>
+</div>
+<?php }
+}
