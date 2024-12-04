ffda3f33452ad1b2b55751bafb93fb780fc9b8f4
Montaso <28571739+Montaso@users.noreply.github.com>
2024-12-04 09:31:24 +0100

    Remove tracking

--- shop/src/modules/bluepayment/src/Install/uninstall.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IF EXISTS `_DB_PREFIX_blue_gateway_transfers`;
-DROP TABLE IF EXISTS `_DB_PREFIX_blue_gateway_transfers_shop`;
-DROP TABLE IF EXISTS `_DB_PREFIX_blue_gateway_channels`;
-DROP TABLE IF EXISTS `_DB_PREFIX_blue_gateway_channels_shop`;
-DROP TABLE IF EXISTS `_DB_PREFIX_blue_transactions`;
